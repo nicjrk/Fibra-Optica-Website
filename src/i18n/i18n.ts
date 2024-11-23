edf3128e45d2d7e71189f09de7f8e0d73d1c06a9
nicjrk <116735056+nicjrk@users.noreply.github.com>
2024-11-23 15:50:30 +0200

    limba

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -8,7 +8,8 @@ i18n
   .use(initReactI18next) // Inițializează react-i18next
   .init({
     resources: translations, // Utilizează resursele de traducere
-    fallbackLng: 'ro', // Limba de rezervă dacă nu se detectează alta
+    lng: 'de', // Setează limba implicită la germană
+    fallbackLng: 'de', // Limba de rezervă este germană
     interpolation: {
       escapeValue: false, // React deja tratează XSS
     },
